58b04d2ca15e8da368bde054e1f572242f21ce9e
Fernando F Costa <ffcostaa@outlook.com>
2019-05-12 21:27:42 -0300

    Updates

---
+++ precache-manifest.c941f8cba130df9cbd78498b5a1ae85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b1783478427c62fa07be9d979c0841",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d9875b4f177bbdf15cea",
+    "url": "/react-gh-pages/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "46dbd8f9a82e66080e56",
+    "url": "/react-gh-pages/static/css/main.7c5166e0.chunk.css"
+  },
+  {
+    "revision": "d9875b4f177bbdf15cea",
+    "url": "/react-gh-pages/static/js/2.4b2da874.chunk.js"
+  },
+  {
+    "revision": "46dbd8f9a82e66080e56",
+    "url": "/react-gh-pages/static/js/main.e0c8aea1.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.cae92349879c43c936844ca6b9055730.js"
+  "/react-gh-pages/precache-manifest.c941f8cba130df9cbd78498b5a1ae85b.js"
 );
 
 self.addEventListener('message', (event) => {
